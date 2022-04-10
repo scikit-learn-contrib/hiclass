33e81ce655df6e07ccd58bcf5ae5bd1f28939414
Fabio <miranda.fmm@gmail.com>
2022-04-11 01:32:03 +0200

    Add function create digraph

--- hiclass/LocalClassifierPerLevel.py
+++ hiclass/LocalClassifierPerLevel.py
@@ -4,6 +4,7 @@ Local classifier per level approach.
 Numeric and string output labels are both handled.
 """
 import logging
+import networkx as nx
 import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.metrics import euclidean_distances
@@ -78,6 +79,51 @@ class LocalClassifierPerLevel(BaseEstimator):
         # Create and configure logger
         self._create_logger()
 
+        # Avoids creating more columns in prediction if edges are a->b and b->c,
+        # which would generate the prediction a->b->c
+        self._disambiguate()
+
+        # Create DAG from self.y_ and store to self.hierarchy_
+        self._create_digraph()
+
+        # If user passes edge_list, then export
+        # DAG to CSV file to visualize with Gephi
+        self._export_digraph()
+
+        # # Assert that graph is directed acyclic
+        # self._assert_digraph_is_dag()
+        #
+        # # If y is 1D, convert to 2D for binary policies
+        # self._convert_1d_y_to_2d()
+        #
+        # # Initialize policy
+        # self._initialize_binary_policy()
+        #
+        # # Detect root(s) and add artificial root to DAG
+        # self._add_artificial_root()
+        #
+        # # Initialize local classifiers in DAG
+        # self._initialize_local_classifiers()
+        #
+        # # Fit local classifiers in DAG
+        # if self.n_jobs > 1:
+        #     self._fit_digraph_parallel()
+        # else:
+        #     self._fit_digraph()
+        #
+        # # TODO: Store the classes seen during fit
+        #
+        # # TODO: Add function to allow user to change local classifier
+        #
+        # # TODO: Add parameter to receive hierarchy as parameter in constructor
+        #
+        # # TODO: Add support to empty labels in some levels
+        #
+        # # TODO: Parallelize fit
+        #
+        # # Delete unnecessary variables
+        # self._clean_up()
+
         # Return the classifier
         return self
 
@@ -126,3 +172,66 @@ class LocalClassifierPerLevel(BaseEstimator):
 
         # Add ch to logger
         self.logger_.addHandler(ch)
+
+    def _disambiguate(self):
+        self.separator_ = "::HiClass::Separator::"
+        if self.y_.ndim == 2:
+            new_y = []
+            for i in range(self.y_.shape[0]):
+                row = [self.y_[i, 0]]
+                for j in range(1, self.y_.shape[1]):
+                    row.append(str(row[-1]) + self.separator_ + str(self.y_[i, j]))
+                new_y.append(np.asarray(row, dtype=np.str_))
+            self.y_ = np.array(new_y)
+
+    def _create_digraph(self):
+        # Create DiGraph
+        self.hierarchy_ = nx.DiGraph()
+
+        # Save dtype of y_
+        self.dtype_ = self.y_.dtype
+
+        # 1D disguised as 2D
+        if self.y_.ndim == 2 and self.y_.shape[1] == 1:
+            self.logger_.info("Converting y to 1D")
+            self.y_ = self.y_.flatten()
+
+        # Check dimension of labels
+        if self.y_.ndim == 2:
+            # 2D labels
+            # Create max_levels variable
+            self.max_levels_ = self.y_.shape[1]
+            rows, columns = self.y_.shape
+            self.logger_.info(f"Creating digraph from {rows} 2D labels")
+            for row in range(rows):
+                for column in range(columns - 1):
+                    self.hierarchy_.add_edge(
+                        self.y_[row, column], self.y_[row, column + 1]
+                    )
+
+        elif self.y_.ndim == 1:
+            # 1D labels
+            # Create max_levels_ variable
+            self.max_levels_ = 1
+            self.logger_.info(f"Creating digraph from {self.y_.size} 1D labels")
+            for label in self.y_:
+                self.hierarchy_.add_node(label)
+
+        else:
+            # Unsuported dimension
+            self.logger_.error(f"y with {self.y_.ndim} dimensions detected")
+            raise ValueError(
+                f"Creating graph from y with {self.y_.ndim} dimensions is not supported"
+            )
+
+    def _export_digraph(self):
+        # Check if edge_list is set
+        if self.edge_list:
+            # Add quotes to all nodes in case the text has commas
+            mapping = {}
+            for node in self.hierarchy_:
+                mapping[node] = '"{}"'.format(node.split(self.separator_)[-1])
+            hierarchy = nx.relabel_nodes(self.hierarchy_, mapping, copy=True)
+            # Export DAG to CSV file
+            self.logger_.info(f"Writing edge list to file {self.edge_list}")
+            nx.write_edgelist(hierarchy, self.edge_list, delimiter=",")
