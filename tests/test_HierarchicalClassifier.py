8229de751b8cffdc7297297beea4204b1134773e
Fabio <miranda.fmm@gmail.com>
2022-04-11 03:17:23 +0200

    Add test for integers for method _disambiguate

--- tests/test_HierarchicalClassifier.py
+++ tests/test_HierarchicalClassifier.py
@@ -5,15 +5,30 @@ from hiclass.HierarchicalClassifier import HierarchicalClassifier
 
 
 @pytest.fixture
-def ambiguous_node():
+def ambiguous_node_str():
     graph = HierarchicalClassifier()
     graph.y_ = np.array([["a", "b"], ["b", "c"]])
     return graph
 
 
-def test_disambiguate(ambiguous_node):
+def test_disambiguate_str(ambiguous_node_str):
     ground_truth = np.array(
         [["a", "a::HiClass::Separator::b"], ["b", "b::HiClass::Separator::c"]]
     )
-    ambiguous_node._disambiguate()
-    assert_array_equal(ground_truth, ambiguous_node.y_)
+    ambiguous_node_str._disambiguate()
+    assert_array_equal(ground_truth, ambiguous_node_str.y_)
+
+
+@pytest.fixture
+def ambiguous_node_int():
+    graph = HierarchicalClassifier()
+    graph.y_ = np.array([[1, 2], [2, 3]])
+    return graph
+
+
+def test_disambiguate_int(ambiguous_node_int):
+    ground_truth = np.array(
+        [["1", "1::HiClass::Separator::2"], ["2", "2::HiClass::Separator::3"]]
+    )
+    ambiguous_node_int._disambiguate()
+    assert_array_equal(ground_truth, ambiguous_node_int.y_)
