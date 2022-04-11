f8edbe254e66b1cda3c376c39302ca3b57729e7e
Fabio <miranda.fmm@gmail.com>
2022-04-11 02:25:56 +0200

    Update docstrings for init methods

--- hiclass/LocalClassifierPerParentNode.py
+++ hiclass/LocalClassifierPerParentNode.py
@@ -37,6 +37,42 @@ class LocalClassifierPerParentNode(BaseEstimator, HierarchicalClassifier):
     for each parent node of the class hierarchy.
     """
 
+    def __init__(
+        self,
+        local_classifier: BaseEstimator = None,
+        verbose: int = 0,
+        edge_list: str = None,
+        replace_classifiers: bool = True,
+        n_jobs: int = 1,
+    ):
+        """
+        Initialize a local classifier per parent node.
+
+        Parameters
+        ----------
+        local_classifier : BaseEstimator, default=LogisticRegression
+            The local_classifier used to create the collection of local classifiers. Needs to have fit, predict and
+            clone methods.
+        verbose : int, default=0
+            Controls the verbosity when fitting and predicting.
+            See https://verboselogs.readthedocs.io/en/latest/readme.html#overview-of-logging-levels
+            for more information.
+        edge_list : str, default=None
+            Path to write the hierarchy built.
+        replace_classifiers : bool, default=True
+            Turns on (True) the replacement of a local classifier with a constant classifier when trained on only
+            a single unique class.
+        n_jobs : int, default=1
+            The number of jobs to run in parallel. Only :code:`fit` is parallelized.
+        """
+        super().__init__(
+            local_classifier=local_classifier,
+            verbose=verbose,
+            edge_list=edge_list,
+            replace_classifiers=replace_classifiers,
+            n_jobs=n_jobs,
+        )
+
     def fit(self, X, y):
         """
         Fit a local classifier per parent node.
