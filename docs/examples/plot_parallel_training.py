b7b868a557ce730cfd9138aa65c52daa821ee007
Fabio <miranda.fmm@gmail.com>
2022-06-17 19:48:18 +0200

    Increase max_iter

--- docs/examples/plot_parallel_training.py
+++ docs/examples/plot_parallel_training.py
@@ -7,7 +7,8 @@ Parallel Training
 Larger datasets require more time for training.
 While by default the models in HiClass are trained using a single core,
 it is possible to train each local classifier in parallel by leveraging the library Ray [1]_.
-In this example, we demonstrate how to train a hierarchical classifier in parallel using a mock dataset from Kaggle [2]_.
+In this example, we demonstrate how to train a hierarchical classifier in parallel,
+using all the cores available on a mock dataset from Kaggle [2]_.
 
 .. [1] https://www.ray.io/
 .. [2] https://www.kaggle.com/datasets/kashnitsky/hierarchical-text-classification
@@ -49,7 +50,7 @@ download(training_data_url, training_data_path)
 training_data = pd.read_csv(training_data_path).fillna(" ")
 
 # We will use logistic regression classifiers for every parent node
-lr = LogisticRegression()
+lr = LogisticRegression(max_iter=1000)
 
 pipeline = Pipeline(
     [
