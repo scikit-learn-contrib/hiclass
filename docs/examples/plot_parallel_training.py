b5d1cd035c87e5f5ee0dc8b87372dd257ebfe30c
Fabio <miranda.fmm@gmail.com>
2022-06-25 00:13:40 +0200

    Add n_jobs to introduction

--- docs/examples/plot_parallel_training.py
+++ docs/examples/plot_parallel_training.py
@@ -8,7 +8,8 @@ Larger datasets require more time for training.
 While by default the models in HiClass are trained using a single core,
 it is possible to train each local classifier in parallel by leveraging the library Ray [1]_.
 In this example, we demonstrate how to train a hierarchical classifier in parallel,
-using all the cores available, on a mock dataset from Kaggle [2]_.
+setting the parameter :literal:`n_jobs` to use all the cores available. Training
+is performed on a mock dataset from Kaggle [2]_.
 
 .. [1] https://www.ray.io/
 .. [2] https://www.kaggle.com/datasets/kashnitsky/hierarchical-text-classification
