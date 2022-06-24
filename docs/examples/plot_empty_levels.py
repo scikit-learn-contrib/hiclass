df8f451670a5276a4059de3dec8859349df52930
Fabio <miranda.fmm@gmail.com>
2022-06-25 01:52:02 +0200

    Update example

--- docs/examples/plot_empty_levels.py
+++ docs/examples/plot_empty_levels.py
@@ -5,19 +5,19 @@ Different Number of Levels
 ==========================
 
 HiClass supports different number of levels in the hierarchy.
-For this example, we will train a local classifier per parent node
+For this example, we will train a local classifier per node
 with a hierarchy similar to the following image:
 
-.. figure:: ../algorithms/local_classifier_per_parent_node.svg
+.. figure:: ../algorithms/local_classifier_per_node.svg
    :align: center
 """
 from sklearn.linear_model import LogisticRegression
 
-from hiclass import LocalClassifierPerParentNode
+from hiclass import LocalClassifierPerNode
 
 # Define data
-X_train = [[1], [2], [3], [4], [5]]
-X_test = [[5], [4], [3], [2], [1]]
+X_train = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+X_test = [[9, 10], [7, 8], [5, 6], [3, 4], [1, 2]]
 Y_train = [
     ["Bird"],
     ["Reptile", "Snake"],
@@ -28,7 +28,7 @@ Y_train = [
 
 # Use random forest classifiers for every node
 rf = LogisticRegression()
-classifier = LocalClassifierPerParentNode(local_classifier=rf)
+classifier = LocalClassifierPerNode(local_classifier=rf)
 
 # Train local classifier per node
 classifier.fit(X_train, Y_train)
