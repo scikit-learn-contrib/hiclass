3b02e6114f85af4eede983e575c609e16094337a
Fabio <miranda.fmm@gmail.com>
2022-09-09 17:13:12 +0200

    Initial test with basic config

--- benchmarks/consumer_complaints/scripts/tune.py
+++ benchmarks/consumer_complaints/scripts/tune.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
-
-@hydra.main(config_path="conf", config_name="config")
-def tune(cfg: DictConfig) -> float:
-    print(cfg.pretty())
-    return 0
-
+@hydra.main(config_path="..", config_name="config")
+def my_app(cfg : DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
-    tune()
+    my_app()
