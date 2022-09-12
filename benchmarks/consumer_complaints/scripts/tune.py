f1191007a6abea50b23216e222c87a88e7cb7911
Fabio <miranda.fmm@gmail.com>
2022-09-12 18:09:36 +0200

    Fix tune.py to run locally

--- benchmarks/consumer_complaints/scripts/tune.py
+++ benchmarks/consumer_complaints/scripts/tune.py
@@ -1,9 +1,11 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-@hydra.main(config_path="..", config_name="config")
-def my_app(cfg : DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+@hydra.main(config_path="../configs", config_name="logistic_regression")
+def optimize(cfg : DictConfig) -> float:
+    classifier__solver: str = cfg.classifier__solver
+    classifier__max_iter: int = cfg.classifier__max_iter
+    return 1
 
 if __name__ == "__main__":
-    my_app()
+    optimize()
