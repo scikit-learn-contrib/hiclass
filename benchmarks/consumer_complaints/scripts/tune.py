0938835fa6704da04a97af2ec6408c1bd8030a81
Fabio <miranda.fmm@gmail.com>
2022-09-22 19:00:46 +0200

    Add slurm

--- benchmarks/consumer_complaints/scripts/tune.py
+++ benchmarks/consumer_complaints/scripts/tune.py
@@ -1,15 +1,21 @@
-import submitit
-from time import sleep
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-def add(a, b):
-    sleep(5)
-    return a + b
 
-# executor is the submission interface (logs are dumped in the folder)
-executor = submitit.AutoExecutor(folder="log_test")
-# set timeout in min, and partition for running the job
-executor.update_parameters(timeout_min=1, slurm_partition="magic", slurm_array_parallelism=2)
-a = [1, 2, 3, 4]
-b = [10, 20, 30, 40]
-jobs = executor.map_array(add, a, b)  # just a list of jobs
-print([job.result() for job in jobs])
+def optimize_lightgbm(cfg: DictConfig) -> float:
+    raise NotImplementedError("lightgbm")
+
+
+@hydra.main(config_path="../configs", config_name="logistic_regression")
+def optimize(cfg: DictConfig) -> float:
+    classifier: str = cfg.classifier
+    if classifier == "lightgbm":
+        return optimize_lightgbm(cfg)
+    # raise NotImplementedError(cfg)
+    # classifier__solver: str = cfg.classifier__solver
+    # classifier__max_iter: int = cfg.classifier__max_iter
+    return 1
+
+
+if __name__ == "__main__":
+    optimize()
