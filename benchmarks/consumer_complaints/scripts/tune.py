be9ec14c8d38a43e10cf6f64e0b29c0b1177be9e
Fabio <miranda.fmm@gmail.com>
2022-09-27 12:48:40 +0200

    Add initial code for lightgbm cross validation

--- benchmarks/consumer_complaints/scripts/tune.py
+++ benchmarks/consumer_complaints/scripts/tune.py
@@ -1,19 +1,59 @@
+from os import getcwd, chdir
+from typing import TextIO
+
 import hydra
-from omegaconf import DictConfig, OmegaConf
+import pandas as pd
+from lightgbm import LGBMClassifier
+from omegaconf import DictConfig
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import cross_val_score
+
+from hiclass.metrics import f1
+
+
+def load_dataframe(path: TextIO) -> pd.DataFrame:
+    """
+    Load a dataframe from a CSV file.
+
+    Parameters
+    ----------
+    path : TextIO
+        Path to CSV file.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Loaded dataframe.
+    """
+    return pd.read_csv(path, compression="infer", header=0, sep=",", low_memory=False)
 
 
 def optimize_lightgbm(cfg: DictConfig) -> float:
-    raise NotImplementedError("lightgbm")
+    local_classifier = LGBMClassifier(
+        n_jobs=cfg.n_jobs,
+        num_leaves=cfg.num_leaves,
+        min_data_in_leaf=cfg.min_data_in_leaf,
+    )
+    chdir("../../../..")
+    # raise NotImplementedError(getcwd())
+    x_train = load_dataframe(cfg.x_train).squeeze()
+    y_train = load_dataframe(cfg.y_train)
+    score = cross_val_score(local_classifier, x_train, y_train, scoring=make_scorer(f1))
+    return score
+
+
+def optimize_logistic_regression(cfg: DictConfig) -> float:
+    raise NotImplementedError(cfg)
 
 
 @hydra.main(config_path="../configs", config_name="logistic_regression")
 def optimize(cfg: DictConfig) -> float:
     classifier: str = cfg.classifier
-    if classifier == "lightgbm":
-        return optimize_lightgbm(cfg)
-    # raise NotImplementedError(cfg)
-    # classifier__solver: str = cfg.classifier__solver
-    # classifier__max_iter: int = cfg.classifier__max_iter
+    optimizers = {
+        "lightgbm": optimize_lightgbm,
+        "logistic_regression": optimize_logistic_regression,
+    }
+    return optimizers[classifier](cfg)
     return 1
 
 
