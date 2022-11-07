1e1234f9d1bfd1bf74b050cc4118dff3a98ac7d2
Fabio <miranda.fmm@gmail.com>
2022-11-07 11:28:56 +0100

    Test black

--- hiclass/HierarchicalClassifier.py
+++ hiclass/HierarchicalClassifier.py
@@ -131,7 +131,7 @@ class HierarchicalClassifier(abc.ABC):
         # and convert them to np.ndarray if need be
 
         self.X_, self.y_ = self._validate_data(
-            X, y, multi_output=True, accept_sparse="csr"
+            X, y, multi_output=True, accept_sparse='csr'
         )
 
         if sample_weight is not None:
