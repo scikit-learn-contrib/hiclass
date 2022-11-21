17e50f58982ab7aae9c4b31b48d3b932dcf8ac0f
Fabio <miranda.fmm@gmail.com>
2022-11-21 16:26:15 +0100

    Add function test_fungi

--- tests/test_RealData.py
+++ tests/test_RealData.py
@@ -1,13 +1,16 @@
 import os
+from multiprocessing import cpu_count
 from os.path import exists
 
 import pytest
+from joblib import parallel_backend
+from sklearn.linear_model import LogisticRegression
 
 from hiclass import (
     LocalClassifierPerNode,
-    LocalClassifierPerParentNode,
-    LocalClassifierPerLevel,
 )
+from hiclass.metrics import f1
+from tests.conftest import get_taxonomy
 
 try:
     import skbio
@@ -17,16 +20,29 @@ else:
     skbio_installed = True
 
 try:
-    from hitac._utils import compute_possible_kmers
+    from hitac._utils import (
+        compute_possible_kmers,
+        _extract_reads,
+        compute_frequencies,
+        extract_qiime2_taxonomy,
+    )
 except ImportError:
     hitac_installed = False
 else:
     hitac_installed = True
 
 
+try:
+    from q2_types.feature_data import DNAIterator
+except ImportError:
+    qiime2_installed = False
+else:
+    qiime2_installed = True
+
+
 @pytest.mark.skipif(
-    not exists("tests/fixtures/fungi_train.csv")
-    or not exists("tests/fixtures/fungi_test.csv"),
+    not exists("tests/fixtures/fungi_train.fasta")
+    or not exists("tests/fixtures/fungi_test.fasta"),
     reason="dataset not available",
 )
 @pytest.mark.skipif(
@@ -38,13 +54,47 @@ else:
 )
 @pytest.mark.skipif(not skbio_installed, reason="scikit-bio not installed")
 @pytest.mark.skipif(not hitac_installed, reason="hitac not installed")
+@pytest.mark.skipif(not qiime2_installed, reason="qiime2 not installed")
 @pytest.mark.parametrize(
     "model",
     [
         LocalClassifierPerNode(),
-        LocalClassifierPerParentNode(),
-        LocalClassifierPerLevel(),
+        # LocalClassifierPerParentNode(),
+        # LocalClassifierPerLevel(),
     ],
 )
 def test_fungi(model):
-    assert False
+    # Variables
+    train = "tests/fixtures/fungi_train.fasta"
+    test = "tests/fixtures/fungi_test.fasta"
+    kmer_size = 6
+    alphabet = "ACGT"
+    threads = min(cpu_count(), 12)
+    logistic_regression_parameters = {
+        "solver": "liblinear",
+        "multi_class": "auto",
+        "class_weight": "balanced",
+        "random_state": 42,
+        "max_iter": 10000,
+        "verbose": 0,
+        "n_jobs": 1,
+    }
+
+    # Training
+    kmers = compute_possible_kmers(kmer_size=kmer_size, alphabet=alphabet)
+    train = DNAIterator(skbio.read(str(train), format="fasta", constructor=skbio.DNA))
+    training_ids, training_sequences = _extract_reads(train)
+    x_train = compute_frequencies(training_sequences, kmers, threads=threads)
+    y_train = get_taxonomy(training_ids)
+    lr = LogisticRegression(**logistic_regression_parameters)
+    model = model.set_params(local_classifier=lr, n_jobs=threads)
+    with parallel_backend("threading", n_jobs=threads):
+        model.fit(x_train, y_train)
+
+    # Testing
+    test = DNAIterator(skbio.read(str(test), format="fasta", constructor=skbio.DNA))
+    test_ids, test_sequences = _extract_reads(test)
+    x_test = compute_frequencies(test_sequences, kmers, threads)
+    y_test = get_taxonomy(test_ids)
+    predictions = model.predict(x_test)
+    assert f1(y_true=y_test, y_pred=predictions) == 1.0
