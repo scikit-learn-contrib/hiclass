3571bf312d239e38c3dd7beaddec75fd3a3d968a
mirand863 <miranda.fmm@gmail.com>
2022-12-14 11:10:09 +0100

    Update hiclass/LocalClassifierPerNode.py

--- hiclass/LocalClassifierPerNode.py
+++ hiclass/LocalClassifierPerNode.py
@@ -74,8 +74,6 @@ class LocalClassifierPerNode(BaseEstimator, HierarchicalClassifier):
         n_jobs : int, default=1
             The number of jobs to run in parallel. Only :code:`fit` is parallelized.
             If :code:`Ray` is installed it is used, otherwise it defaults to :code:`Joblib`.
-        warm_start : bool, default=False
-            When set to `True`, reuse the solution of the previous call to fit and add more estimators for new nodes, otherwise, just fit a whole new DAG. See :ref:`Fitting additional nodes` for more information.
         """
         super().__init__(
             local_classifier=local_classifier,
