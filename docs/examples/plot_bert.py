5777bca1756c92dac200d05b59c92cc2a0645c56
mirand863 <miranda.fmm@gmail.com>
2023-01-24 13:57:02 +0100

    Update plot_bert.py

--- docs/examples/plot_bert.py
+++ docs/examples/plot_bert.py
@@ -7,7 +7,7 @@ BERT sklearn
 In order to use `bert-sklearn <https://github.com/charles9n/bert-sklearn>`_ with HiClass, some of scikit-learns checks need to be disabled.
 The reason is that BERT expects text as input for the features, but scikit-learn expects numerical features.
 Hence, the checks will fail.
-To disable scikit-learn's checks, we can simply use the parameter `bert=True` in the constructor of the local hierarchical classifier.
+To disable scikit-learn's checks, we can simply use the parameter :literal:`bert=True` in the constructor of the local hierarchical classifier.
 """
 from bert_sklearn import BertClassifier
 from hiclass import LocalClassifierPerParentNode
