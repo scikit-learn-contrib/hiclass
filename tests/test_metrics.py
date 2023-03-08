a1e7d9cc649c83c608e1ae6b6e1a37a2be4c38c7
Fabio <miranda.fmm@gmail.com>
2023-03-08 15:52:21 +0100

    Add tests for numpy arrays and lists

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,61 +5,108 @@ from pytest import approx
 from hiclass import metrics
 
 
-def test_unmatched_lengths():
-    y_true = np.array([[1, 2, 3], [1, 2, 4], [1, 5, 6], [1, 5, 8]], dtype=np.int32)
-    y_pred = np.array([[1, 2, 3], [1, 2, 4]], dtype=np.int32)
+def test_unmatched_lengths_2d_list():
+    y_true = [[1, 2, 3], [1, 2, 4], [1, 5, 6], [1, 5, 8]]
+    y_pred = [[1, 2, 3], [1, 2, 4]]
     with pytest.raises(AssertionError):
         metrics.precision(y_true, y_pred)
 
 
-def test_precision_micro():
+def test_unmatched_lengths_2d_np_array():
+    y_true = np.array([[1, 2, 3], [1, 2, 4], [1, 5, 6], [1, 5, 8]])
+    y_pred = np.array([[1, 2, 3], [1, 2, 4]])
+    with pytest.raises(AssertionError):
+        metrics.precision(y_true, y_pred)
+
+
+def test_precision_micro_2d_list():
+    y_true = [[1, 2, 3, 4], [1, 2, 5, 6]]
+    y_pred = [[1, 2, 5, 6], [1, 2, 3, 4]]
+    assert metrics.precision(y_true, y_pred, "micro") == 0.5
+    assert metrics.precision(y_true, y_true, "micro") == 1
+
+
+def test_precision_micro_2d_np_array():
     y_true = np.array([[1, 2, 3, 4], [1, 2, 5, 6]])
     y_pred = np.array([[1, 2, 5, 6], [1, 2, 3, 4]])
     assert metrics.precision(y_true, y_pred, "micro") == 0.5
     assert metrics.precision(y_true, y_true, "micro") == 1
 
 
-def test_precision_macro():
+def test_precision_macro_2d_list():
+    y_true = [[1, 2, 3, 4], [1, 2, 5, 6]]
+    y_pred = [[1, 5, 6, 7], [1, 2, 3, 4]]
+    assert metrics.precision(y_true, y_pred, "macro") == 0.375
+    assert metrics.precision(y_true, y_true, "macro") == 1
+
+
+def test_precision_macro_2d_np_array():
     y_true = np.array([[1, 2, 3, 4], [1, 2, 5, 6]])
     y_pred = np.array([[1, 5, 6, 7], [1, 2, 3, 4]])
     assert metrics.precision(y_true, y_pred, "macro") == 0.375
     assert metrics.precision(y_true, y_true, "macro") == 1
 
 
-def test_recall_micro():
+def test_recall_micro_2d_list():
+    y_true = [[1, 2], [1, 2]]
+    y_pred = [[1, 2, 5, 6], [1, 2, 3, 4]]
+    assert metrics.recall(y_true, y_pred, "micro") == 1
+    assert metrics.recall(y_pred, y_true, "micro") == 0.5
+
+
+def test_recall_micro_2d_np_array():
     y_true = np.array([[1, 2], [1, 2]])
     y_pred = np.array([[1, 2, 5, 6], [1, 2, 3, 4]])
     assert metrics.recall(y_true, y_pred, "micro") == 1
     assert metrics.recall(y_pred, y_true, "micro") == 0.5
 
 
-def test_recall_macro():
+def test_recall_macro_2d_list():
+    y_true = [[1, 2], [1, 2]]
+    y_pred = [[1, 5, 6, 7], [1, 2, 3, 4]]
+    assert metrics.recall(y_true, y_pred, "macro") == 0.75
+    assert metrics.recall(y_pred, y_true, "macro") == 0.375
+
+
+def test_recall_macro_2d_np_array():
     y_true = np.array([[1, 2], [1, 2]])
     y_pred = np.array([[1, 5, 6, 7], [1, 2, 3, 4]])
     assert metrics.recall(y_true, y_pred, "macro") == 0.75
     assert metrics.recall(y_pred, y_true, "macro") == 0.375
 
 
-def test_f1_micro():
+def test_f1_micro_2d_list():
+    y_true = [[1, 2, 3, 4], [1, 2, 5, 6]]
+    y_pred = [[1, 2, 5, 6], [1, 2, 3, 4]]
+    assert metrics.f1(y_true, y_pred, "micro") == 0.5
+
+
+def test_f1_micro_2d_np_array():
     y_true = np.array([[1, 2, 3, 4], [1, 2, 5, 6]])
     y_pred = np.array([[1, 2, 5, 6], [1, 2, 3, 4]])
     assert metrics.f1(y_true, y_pred, "micro") == 0.5
 
 
-def test_f1_macro():
+def test_f1_macro_2d_list():
+    y_true = [[1, 2, 3, 4], [1, 2, 5, 6]]
+    y_pred = [[1, 5, 6], [1, 2, 3]]
+    assert 0.4285714 == approx(metrics.f1(y_true, y_pred, "macro"))
+
+
+def test_f1_macro_2d_np_array():
     y_true = np.array([[1, 2, 3, 4], [1, 2, 5, 6]])
     y_pred = np.array([[1, 5, 6], [1, 2, 3]])
     assert 0.4285714 == approx(metrics.f1(y_true, y_pred, "macro"))
 
 
-def test_empty_levels_1():
+def test_empty_levels_2d_1():
     y_true = [["2", "3"], ["1"], ["4", "5", "6"]]
     y_pred = [["1", "", ""], ["2", "3", ""], ["4", "5", "6"]]
     assert metrics.f1(y_true, y_pred) == 0.5
     assert metrics.f1(y_true, y_true) == 1
 
 
-def test_empty_levels_2():
+def test_empty_levels_2d_2():
     y_true = [["1"], ["2", "3"], ["4", "5", "6"]]
     y_pred = [["1", "", ""], ["2", "3", ""], ["4", "5", "6"]]
     assert metrics.f1(y_true, y_pred) == 1
