901d9cce4cb30135e08e579a42c06419046f6b25
Jonathan Haas <jonathan.haas.ger@gmail.com>
2023-03-17 12:44:58 +0100

    Update Multi-Label Documentation

--- docs/examples/plot_multilevel.py
+++ docs/examples/plot_multilevel.py
@@ -6,20 +6,28 @@ Multi-Label Classification
 
 HiClass supports assigning multiple labels to a single sample.
 In this example, we will train a local classifier per node
-with a hierarchy similar to the following image:
+with a hierarchy depicted as the following image:
 
-.. figure:: ../algorithms/local_classifier_per_node_multilevel.png
+.. figure:: ../algorithms/multilabel_hierarchy.svg
    :align: center
 
 Without multi-label classification, a sample can only belong
-to a single class. However, many real-world problems require
-a sample to belong to multiple classes.
-For example, a duck has 2 wings and 2 legs.
-An Human has no wings, but 2 legs.
-A dragonfly has 4 wings and 6 legs.
+to a single class. However, in some cases a sample may belong
+to multiple classes, especially when the classes are not mutually exclusive.
 
-We can encode this information in our Y_train data as follows:
+For example, in the given hierarchy if you want to encode that a sample
+is a mermaid, e.g. half human and half fish, you can assign the labels for
+that data point as follows.
+
+Important to note here is that you need to specify the whole path
+from the root to the most specific nodes for each multi-label, as
+can be seen in the 2nd training example for a minotaur.
+
+As a result, the predictions will be an array of fixed dimensions, with
+empty levels inserted as fillers, as can be seen in the output for the 3rd
+sample.
 """
+
 from sklearn.tree import DecisionTreeClassifier
 
 from hiclass.MultiLabelLocalClassifierPerNode import MultiLabelLocalClassifierPerNode
@@ -28,9 +36,9 @@ from hiclass.MultiLabelLocalClassifierPerNode import MultiLabelLocalClassifierPe
 X_train = [[1, 2], [3, 4], [5, 6]]
 X_test = [[1, 2], [3, 4], [5, 6]]
 Y_train = [
-    [["has_wings", "2"], ["has_legs", "2"]],  # Duck
-    [["has_legs", "2"]],  # Human
-    [["has_wings", "4"], ["has_legs", "6"]],  # Dragonfly
+    [["Mammal", "Human"], ["Fish"]],  # Mermaid
+    [["Mammal", "Human"], ["Mammal", "Bovine"]],  # Minotaur
+    [["Mammal", "Human"]],  # just a Human
 ]
 
 # Use decision tree classifiers for every node
