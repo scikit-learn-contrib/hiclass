eef43e965bd7a7e6c1040b03b819c6aa49e52c7d
Andrea Dodet <an.dodet@gmail.com>
2023-05-15 15:28:45 +0200

    Explicitly declare dtypes in numpy arrays (#90)
    Closes #75
    
    * explicitly declare dtype in numpy arrays
    
    * unlock numpy version

--- docs/examples/plot_empty_levels.py
+++ docs/examples/plot_empty_levels.py
@@ -11,6 +11,7 @@ with a hierarchy similar to the following image:
 .. figure:: ../algorithms/local_classifier_per_node.svg
    :align: center
 """
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 from hiclass import LocalClassifierPerNode
@@ -18,13 +19,16 @@ from hiclass import LocalClassifierPerNode
 # Define data
 X_train = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
 X_test = [[9, 10], [7, 8], [5, 6], [3, 4], [1, 2]]
-Y_train = [
-    ["Bird"],
-    ["Reptile", "Snake"],
-    ["Reptile", "Lizard"],
-    ["Mammal", "Cat"],
-    ["Mammal", "Wolf", "Dog"],
-]
+Y_train = np.array(
+    [
+        ["Bird"],
+        ["Reptile", "Snake"],
+        ["Reptile", "Lizard"],
+        ["Mammal", "Cat"],
+        ["Mammal", "Wolf", "Dog"],
+    ],
+    dtype=object,
+)
 
 # Use random forest classifiers for every node
 rf = LogisticRegression()
