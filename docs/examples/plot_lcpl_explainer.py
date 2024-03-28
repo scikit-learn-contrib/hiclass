84c5cd5f0fdd81eee929199b03f7515d7a5cca96
dniprocat <itunyov@gmail.com>
2024-03-29 00:33:40 +0100

    some edits in readthedocs

--- docs/examples/plot_lcpl_explainer.py
+++ docs/examples/plot_lcpl_explainer.py
@@ -45,7 +45,6 @@ explanations = explainer.explain(x_test)
 print("Explanations are", explanations)
 
 
-
 # Explanations can be easily filtered
 
 # We want
@@ -56,23 +55,23 @@ level = 1
 # Use .sel() method
 level_1_explanations = explanations.sel(level=1)
 
-#print(level_1_explanations)
+# print(level_1_explanations)
 
-#print(level_1_explanations['classes'].values)
+# print(level_1_explanations['classes'].values)
 
 level_1_data = explanations.sel(level=1)
 # Извлекаем классы, соответствующие первому уровню иерархии
 # Выбираем данные для первого уровня иерархии
 
-#print("PSDA\n\n\n\n")
-#print(explanations.sel(level=1)['shap_values'].values)
+# print("PSDA\n\n\n\n")
+# print(explanations.sel(level=1)['shap_values'].values)
 
 classes_level_1 = level_1_data["classes"]
 print(classes_level_1.values)
 
-shp = level_1_data.sel(class_='Cold')['shap_values'].values
-print(shp[:, 4])
-'''
+shp = level_1_data.sel(class_="Cold")["shap_values"].values
+print(shp)
+"""
 #for cl in classes_level_1:
 #    print(cl, level_1_explanations.sel(class_=cl)['shap_values'].values)
 #desired_class = 'Cold_Allergy'
@@ -80,4 +79,4 @@ print(shp[:, 4])
 
 #for a in classes_level_1:
 #    print(a.values)
-'''
+"""
