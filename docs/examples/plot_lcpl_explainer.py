b9e14f1feac50bdff6f30482ccf4c1ca3fedb3b8
dniprocat <itunyov@gmail.com>
2024-04-09 15:07:46 +0200

    Platypus dataset based example added

--- docs/examples/plot_lcpl_explainer.py
+++ docs/examples/plot_lcpl_explainer.py
@@ -6,63 +6,37 @@ Explaining Local Classifier Per Level
 
 A minimalist example showing how to use HiClass Explainer to obtain SHAP values of LCPL model.
 A detailed summary of the Explainer class has been given at Algorithms Overview Section for :ref:`Hierarchical Explainability`.
+SHAP values are calculated based on a synthetic platypus diseases dataset that can be downloaded here.
 """
 from sklearn.ensemble import RandomForestClassifier
-import numpy as np
 from hiclass import LocalClassifierPerLevel, Explainer
+import shap
+from hiclass.datasets import load_platypus
 
-# Define data
-x_train = np.array(
-    [
-        [40.7, 1.0, 1.0, 2.0, 5.0, 2.0, 1.0, 5.0, 34.3],
-        [39.2, 0.0, 2.0, 4.0, 1.0, 3.0, 1.0, 2.0, 34.1],
-        [40.6, 0.0, 3.0, 1.0, 4.0, 5.0, 0.0, 6.0, 27.7],
-        [36.5, 0.0, 3.0, 1.0, 2.0, 2.0, 0.0, 2.0, 39.9],
-    ]
-)
-x_test = np.array([[35.5, 0.0, 1.0, 1.0, 3.0, 3.0, 0.0, 2.0, 37.5]])
-y_train = np.array(
-    [
-        ["Gastrointestinal", "Norovirus", ""],
-        ["Respiratory", "Covid", ""],
-        ["Allergy", "External", "Bee Allergy"],
-        ["Respiratory", "Cold", ""],
-    ]
-)
+# Load train and test splits
+X_train, X_test, Y_train, Y_test = load_platypus()
 
 # Use random forest classifiers for every level
 rfc = RandomForestClassifier()
-lcpl = LocalClassifierPerLevel(local_classifier=rfc, replace_classifiers=False)
+classifier = LocalClassifierPerLevel(local_classifier=rfc, replace_classifiers=False)
 
 # Train local classifiers per level
-lcpl.fit(x_train, y_train)
+classifier.fit(X_train, Y_train)
 
 # Define Explainer
-explainer = Explainer(lcpl, data=x_train, mode="tree")
-explanations = explainer.explain(x_test)
+explainer = Explainer(classifier, data=X_train, mode="tree")
+explanations = explainer.explain(X_test.values)
+print(explanations)
 
-# Print out the results
-print("Explanations are", explanations)
+# Example of filtering for levels and classes using the .sel() method
+# Here we select explanations for "Respiratory" at the first level
+respiratory_idx = classifier.predict(X_test)[:, 0] == "Respiratory"
+shap_filter = {"level": 0, "class_": "Respiratory", "sample": respiratory_idx}
+shap_val_respiratory = explanations.sel(**shap_filter)
 
-
-"""
-Explanations can be easily filtered with .sel() method
-Let's consider level 1 of the LCPL hierarchy
-"""
-
-# Define a level
-level = 1
-
-# Use .sel() method
-level_1_explanations = explanations.sel(level=1)
-
-# print explanations at the level
-print(level_1_explanations)
-
-# Let's see classes at this level
-classes_level_1 = level_1_explanations["classes"]
-print(classes_level_1.values)
-
-# Now, let's see the shapley values for "Cold"
-shp = level_1_explanations.sel(class_="Cold")["shap_values"].values
-print(shp)
+# Plot feature importance for the "Respiratory" class
+shap.plots.violin(
+    shap_val_respiratory.shap_values,
+    feature_names=X_train.columns.values,
+    plot_size=(13, 8),
+)
