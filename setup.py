b68361a0e6693d61c7a135fc32ac244fb1350126
Fábio Malcher Miranda <fabio.miranda@ifap.edu.br>
2026-04-19 18:15:46 -0300

    Remove ray from requirements

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ REQUIRES_PYTHON = ">=3.10,<3.14"
 KEYWORDS = ["hierarchical classification"]
 DACS_SOFTWARE = "https://gitlab.com/dacs-hpi"
 # What packages are required for this module to be executed?
-REQUIRED = ["networkx", "numpy", "scikit-learn>=1.5", "scipy"]
+REQUIRED = ["networkx", "numpy", "scikit-learn>=1.5", "scipy", "requests"]
 
 # What packages are optional?
 # 'fancy feature': ['django'],}
@@ -41,7 +41,7 @@ EXTRAS = {
         "pyfakefs==6.2.0",
         "black==26.3.0",
         "pre-commit==2.20.0",
-        "ray",
+        "ray; python_version < '3.13'",
     ],
 }
 
